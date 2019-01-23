03ec62e520ce5d19cc47fce71a3c7b5a27ecb200
axy <xavdlr@gmail.com>
2019-01-22 21:32:29 -0800

    added

---
+++ Pyber/Pyber.py
@@ -0,0 +1,19 @@
+print("Hello")
+
+ 
+ 
+ %matplotlib inline
+# Dependencies and Setup
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# File to Load (Remember to change these)
+city_data_to_load = "data/city_data.csv"
+ride_data_to_load = "data/ride_data.csv"
+
+# Read the City and Ride Data
+
+# Combine the data into a single dataset
+
+# Display the data table for preview
